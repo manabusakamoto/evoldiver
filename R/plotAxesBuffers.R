486a631ada712d434fe117c512183283b7f79f7e
Manabu Sakamoto <manabusakamoto@samc7001.rdg.ac.uk>
2018-03-06 11:45:04 +0000

    fixed function

--- R/plotAxesBuffers.R
+++ R/plotAxesBuffers.R
@@ -2,7 +2,7 @@ plotAxesBuffers <- function(x, y, ...){
   ax.buff <- axesBuffers(x = x, y = y)
   .xlim <- ax.buff$xlim
   .ylim <- ax.buff$ylim
-  plot(.xlim, .ylim, xlim = .xlim, ylim = .ylim, type = "n", xlab = "Time", ylab = "Net speciation per myr", xaxs = "i", yaxs = "i", axes=F)
+  plot(.xlim, .ylim, xlim = .xlim, ylim = .ylim, type = "n", xaxs = "i", yaxs = "i", axes=F, ...)
   axis(1, at=.xlim, labels=c("",""), lwd.ticks=0)
   axis(1, lwd=0, lwd.ticks=1)
   axis(2, at=.ylim, labels=c("",""), lwd.ticks=0)
