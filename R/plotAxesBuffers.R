0d336f0aec0e4b1bb61dd8878a842c664194bae8
Manabu Sakamoto <manabusakamoto@samc7001.rdg.ac.uk>
2018-03-08 16:34:53 +0000

    added rev.x to function plotAxesBuffers

--- R/plotAxesBuffers.R
+++ R/plotAxesBuffers.R
@@ -1,7 +1,8 @@
-plotAxesBuffers <- function(x, y, ...){
+plotAxesBuffers <- function(x, y, rev.x=NULL, ...){
   ax.buff <- axesBuffers(x = x, y = y)
   .xlim <- ax.buff$xlim
   .ylim <- ax.buff$ylim
+  if(rev.x) .xlim <- rev(.xlim)
   plot(.xlim, .ylim, xlim = .xlim, ylim = .ylim, type = "n", xaxs = "i", yaxs = "i", axes=F, ...)
   axis(1, at=.xlim, labels=c("",""), lwd.ticks=0)
   axis(1, lwd=0, lwd.ticks=1)
