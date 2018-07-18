0110050280fe31942179e36b5fe21f5fc78d96b6
Manabu Sakamoto <manabusakamoto@samc7001.rdg.ac.uk>
2018-07-18 11:35:00 +0100

    changed function coeff.BT

--- R/coeff.BT.R
+++ R/coeff.BT.R
@@ -1,10 +1,21 @@
-coeff.BT <- function(dt, lg){
+coeff.BT <- function(x, data=NULL, header=NULL){
+  # x is BayesTraits log file object
+  # data is input data object with headers
+  # header is vector of variable names
+  if(exists("data") & exists("header")) {
+    warning("Arguments data and header are specified: Taking header argument")
+    v.dt <- header
+  }else{
+    if(!is.null(data) & is.null(header)){
+      v.dt <- names(data)
+      v.dt <- v.dt[-1]
+      v.dt[1] <- "Intercept"
+    }
+    if(!is.null(header) & is.null(data)) v.dt <- header
+  }
   v.lg <- names(lg)
-  v.dt <- names(dt)
   v.lg <- v.lg[grepl("^Alpha", v.lg) | grepl("^Beta", v.lg)]
   lg.dm <- lg[, v.lg, with=F]
-  v.dt <- v.dt[-1]
-  v.dt[1] <- "Intercept"
-  setnames(lg.dm, v.dt)
+  if(exists("v.dt")) setnames(lg.dm, v.dt)
   return(lg.dm)
 }
