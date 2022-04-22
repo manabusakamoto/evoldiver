45635758700b5ec6a4870a2d8899f5f32bde9db5
Manabu <manabu@cosmbp014.home>
2022-04-22 20:11:47 +0100

    moditied function rateScalar

--- R/rateScalar.R
+++ R/rateScalar.R
@@ -1,1 +1,14 @@
-rateScalar <- function(phy, phy.ref){phy$edge.length / phy.ref$edge.length}
+rateScalar <- function(phy, phy.ref){
+  if(length(phy) > 1){
+    function()
+    r <- lapply(phy, rateScalar.internal, phy.ref = phy.ref)
+    r <- lapply(r, function(x){data.table(t(x))})
+    r <- rbindlist(r)
+    r.med <- unlist(r[, lapply(.SD, median)])
+    r.mean <- unlist(r[, lapply(.SD, mean)])
+    rates <- list(RateScalars = r, RateScalars_Median=r.med, RateScalars_Mean=r.mean)
+  }else{
+    rates <- rateScalar.internal(phy, phy.ref)
+  }
+  return(rates)
+}
