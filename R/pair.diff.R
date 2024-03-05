5e4dbeb3ffcdb234dfc71bd043bf207b584cf996
Manabu Sakamoto <manabu.sakamoto@gmail.com>
2024-03-05 09:43:36 +0000

    debug

--- R/pair.diff.R
+++ R/pair.diff.R
@@ -2,7 +2,7 @@ pair.diff <- function(x){
   df <- matrix(ncol = length(x), nrow = length(x))
   for(i in 1:length(x)) {
     # i <- 1
-    .x <- [i]
+    .x <- x[i]
     df[, i] <- x - .x
   }
   colnames(df) <- rownames(df) <- names(x)
